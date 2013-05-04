4a2f89f0406ba50acc1eea6a93799d7b3758f3c4
gparlakov <gparlakov@gmail.com>
2013-05-04 08:02:35 -0700

    Hight Quality Code 10 - Code optimization done fist 3 tasks (required) left is mandatory

---
+++ 10.CodeTuningAndOptimization/PerformanceMathMethods/PerformanceMathMethods.cs
@@ -0,0 +1,84 @@
+﻿/// <summary>
+/// Write a program to compare the performance of square root, 
+/// natural logarithm, sinus for float, double and decimal values.
+/// </summary>
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PerformanceMathMethods
+{
+    class PerformanceMathMethods
+    {
+        const int Repetitions = 1000000;
+
+        static void Main()
+        {
+            Stopwatch timer = new Stopwatch();
+            float value = 500000;
+            Sqrt(value, ref timer);
+            Console.WriteLine(
+                "Time in milliseconds for Sqrt {0} times with float," + 
+                " double and decimal(in that order) values.",
+                Repetitions);
+            Console.WriteLine(Sqrt(value, ref timer));
+            Console.WriteLine(Sqrt((double)value, ref timer));
+            Console.WriteLine(Sqrt((decimal)value, ref timer));
+
+            Console.WriteLine(
+                "Time in milliseconds for Sqrt {0} times with float," +
+                " double and decimal(in that order) values.",
+                Repetitions); Console.WriteLine(NaturalLogarithm(value, ref timer));
+            Console.WriteLine(NaturalLogarithm((double)value, ref timer));
+            Console.WriteLine(NaturalLogarithm((decimal)value, ref timer));
+
+            Console.WriteLine(
+                "Time in milliseconds for Sqrt {0} times with float," +
+                " double and decimal(in that order) values.",
+                Repetitions); Console.WriteLine(Sin(value, ref timer));
+            Console.WriteLine(Sin((double)value, ref timer));
+            Console.WriteLine(Sin((decimal)value, ref timer));
+        }
+
+        private static long Sqrt(dynamic value, ref Stopwatch timer)
+        {
+            dynamic result = value;
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i <= Repetitions; i++)
+            {
+                result = Math.Sqrt((double)value);
+            }
+            timer.Stop();
+            return timer.ElapsedMilliseconds;
+        }
+
+        private static long NaturalLogarithm(dynamic value, ref Stopwatch timer)
+        {
+            dynamic result = value;
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i <= Repetitions; i++)
+            {
+                result = Math.Log((double)value, Math.E);
+            }
+            timer.Stop();
+            return timer.ElapsedMilliseconds;
+        }
+
+        private static long Sin(dynamic value, ref Stopwatch timer)
+        {
+            dynamic result = value;
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i <= Repetitions; i++)
+            {
+                result = Math.Sin((double)value);
+            }
+            timer.Stop();
+            return timer.ElapsedMilliseconds;
+        }
+
+    }
+}
---
+++ 10.CodeTuningAndOptimization/PerformanceOfSortAlgos/PerformanceOfSortAlgos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace PerformanceOfSortAlgos
+{
+    class PerformanceOfSortAlgos
+    {
+        static void Main()
+        {
+            string[] letters = GenLettersDictionary();
+        }
+
+        private static string[] GenRandomString(int count, string[] dictionary)
+        {
+            Random randomGenarator = new Random();
+            string[] result = new string[count];
+            for (int i = 0; i < result.Length; i++)
+            {
+                int wordsCount = 1 + randomGenarator.Next(10);                
+                int dictionaryLength = dictionary.Length;
+                StringBuilder adder = new StringBuilder();
+                for (int j = 0; j < wordsCount; j++)
+                {
+                    adder.Append(dictionary[randomGenarator.Next(dictionaryLength)]);
+                }
+
+                result[i] = adder.ToString();
+            }
+
+            return result;
+        }
+
+        private static string[] GenLettersDictionary()
+        {
+            string[] result = new string[52];
+            for (char ch = 'a'; ch <= 'a' + 'z'; ch += (char)2)
+            {
+                result[ch - 'a'] = ((char)ch).ToString();
+                result[ch - 'a' + 'A' - 1] = ((char)(ch + 'a')).ToString();
+            }
+
+            return result;
+        }
+    }
+}
