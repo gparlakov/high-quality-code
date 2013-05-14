0605f78807614c7006f4f7f4028c47cd856acf0e
gparlakov <gparlakov@gmail.com>
2013-05-14 14:29:26 -0700

    Test Driven Development HW - Almost finished = only compare hands left but no time for now

---
+++ 12. Test-Driven-Development-Demo-Homework/PokerHands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poker
+{
+    public enum PokerHands
+    {
+        HighCard = 1,
+        OnePair = 2,
+        TwoPair = 3,
+        ThreeOfAKind = 4,
+        Straight = 5,
+        Flush = 6,
+        FullHouse = 7,
+        FourOfAKind = 8,
+        StraightFlush = 9
+    }
+}
